7070aa8dd58c3c0ba522a7e1d9ba8b9f3802735a
Tomonori Tamagawa <xtuaok@gmail.com>
2015-06-24 18:18:12 +0900

    Add new jobs MCH,DRK and AST.
    minor fix, EnmityOvelay.pid should not be static.

--- EnmityPlugin/EnmityOverlay.cs
+++ EnmityPlugin/EnmityOverlay.cs
@@ -32,7 +32,7 @@ namespace Tamagawa.EnmityPlugin
         private static string targetSignature64 = "f30f5ec6f30f11442420e80191ffff488b05";
         private static string targetSignature32 = "750e85d2750ab9";
         private static int targetOffset = 88;
-        private static int pid = 0;
+        private int pid = 0;
         private IntPtr charmapAddress = IntPtr.Zero;
         private IntPtr targetAddress = IntPtr.Zero;
         private IntPtr hateAddress = IntPtr.Zero;
@@ -339,36 +339,39 @@ namespace Tamagawa.EnmityPlugin
         public enum JobEnum : byte
         {
             UNKNOWN,
-            GLD,
-            PGL,
-            MRD,
-            LNC,
-            ARC,
-            CNJ,
-            THM,
-            CRP,
-            BSM,
-            ARM,
-            GSM,
-            LTW,
-            WVR,
-            ALC,
-            CUL,
-            MIN,
-            BTN,
-            FSH,
-            PLD,
-            MNK,
-            WAR,
-            DRG,
-            BRD,
-            WHM,
-            BLM,
-            ACN,
-            SMN,
-            SCH,
-            ROG,
-            NIN
+            GLD, // 1
+            PGL, // 2
+            MRD, // 3
+            LNC, // 4
+            ARC, // 5
+            CNJ, // 6
+            THM, // 7
+            CRP, // 8
+            BSM, // 9
+            ARM, // 10
+            GSM, // 11
+            LTW, // 12
+            WVR, // 13
+            ALC, // 14
+            CUL, // 15
+            MIN, // 15
+            BTN, // 17
+            FSH, // 18
+            PLD, // 19
+            MNK, // 20
+            WAR, // 21
+            DRG, // 22
+            BRD, // 23
+            WHM, // 24
+            BLM, // 25
+            ACN, // 26
+            SMN, // 27
+            SCH, // 28
+            ROG, // 29
+            NIN, // 30
+            MCH, // 31
+            DRK, // 32
+            AST  // 33
         }
 
         //// 敵視されてるキャラエントリ
