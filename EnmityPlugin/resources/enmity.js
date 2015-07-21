0ffd4adc34a3ca2a9d0d13291ee2b665e640b948
Tomonori Tamagawa <xtuaok@gmail.com>
2015-07-21 16:48:56 +0900

    Rewrite resource html with Vue.

---
+++ EnmityPlugin/resources/enmity.js
@@ -0,0 +1,98 @@
+// ターゲットしてないときは表示しない
+var hideNoTarget = false;
+
+// ロールの定義
+var JobRole = {
+ PLD: 'TANK',
+ WAR: 'TANK',
+ GLD: 'TANK',
+ MRD: 'TANK',
+ DRK: 'TANK',
+
+ CNJ: 'HEALER',
+ WHM: 'HEALER',
+ SCH: 'HEALER',
+ AST: 'HEALER',
+
+ PGL: 'DPS',
+ LNC: 'DPS',
+ ARC: 'DPS',
+ THM: 'DPS',
+ MNK: 'DPS',
+ DRG: 'DPS',
+ BRD: 'DPS',
+ BLM: 'DPS',
+ ACN: 'DPS',
+ SMN: 'DPS',
+ ROG: 'DPS',
+ NIN: 'DPS',
+ MCH: 'DPS',
+};
+
+// ターゲットしてないときのダミーデータ
+var noTarget = {
+  Name: '- none -',
+  MaxHP: '--',
+  CurrentHP: '--',
+  HPPercent: '--',
+  Distance: '--',
+  EffectiveDistance: '--',
+  HorizontalDistance: '--',
+}
+
+// フィルタ
+Vue.filter('jobclass', function (v) {
+ var role = JobRole[v.JobName];
+ if (v.isPet) return "Pet";
+ if (role != null) return role;
+ return "UNKNOWN";
+});
+
+Vue.filter('hpcolor', function (t) {
+  if (t.HPPerecnt > 75) return "green";
+  if (t.HPPerecnt > 50) return "yellow";
+  if (t.HPPerecnt > 25) return "orange";
+  return "red";
+});
+
+Vue.filter('you', function (v) {
+  return v.isMe ? "YOU" : v.Name;
+});
+
+// データ処理
+var enmity = new Vue({
+  el: '#enmity',
+  data: {
+    updated: false,
+    locked: false,
+    collapsed: false,
+    target: null,
+    entries: null,
+    hideNoTarget: ""
+  },
+  attached: function() {
+    document.addEventListener('onOverlayDataUpdate', this.update);
+    document.addEventListener('onOverlayStateUpdate', this.updateState);
+  },
+  detached: function() {
+    document.removeEventListener('onOverlayStateUpdate', this.updateState);
+    document.removeEventListener('onOverlayDataUpdate', this.update);
+  },
+  methods: {
+    update: function(e) {
+      this.updated = true;
+      this.entries = e.detail.Enmity.Entries;
+      this.target  = e.detail.Enmity.Target ? e.detail.Enmity.Target : noTarget;
+      if (hideNoTarget && e.detail.Enmity.Target == null) {
+        this.hideNoTarget = 'hide';
+      }
+    },
+    updateState: function(e) {
+      this.locked = e.detail.isLocked;
+    },
+    toggleCollapse: function() {
+      this.collapsed = !this.collapsed;
+    }
+  }
+});
+
