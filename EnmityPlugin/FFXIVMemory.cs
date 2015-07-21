bf0b93b0edf1c8bde8e05e0d912ddb0d17ff76f5
Tomonori Tamagawa <xtuaok@gmail.com>
2015-07-21 21:47:31 +0900

    Fix memory signature for 3.05 (DX9/DX11)

--- EnmityPlugin/FFXIVMemory.cs
+++ EnmityPlugin/FFXIVMemory.cs
@@ -33,11 +33,11 @@ namespace Tamagawa.EnmityPlugin
         private const string charmapSignature32 = "81FEFFFF0000743581FE58010000732D8B3CB5";
         private const string charmapSignature64 = "48C1E8033DFFFF0000742B3DA80100007324488D0D";
         private const string targetSignature32  = "750E85D2750AB9";
-        private const string targetSignature64  = "4883C4205FC3483935285729017520483935";
-        private const string enmitySignature32  = "E8D1A73300B9";
-        private const string enmitySignature64  = "9F3C00488D0DA5DF3C01E840EE3F00488D0D";
-        private const string aggroSignature32   = "E86DCB2D00B9";
-        private const string aggroSignature64   = "83EC28E8275179003D000000E07412488D0D";
+        private const string targetSignature64  = "29017520483935";
+        private const string enmitySignature32  = "02E801A83300B9";
+        private const string enmitySignature64  = "488D0DB5C13C01E820EF3F00488D0D";
+        private const string aggroSignature32   = "02E84DCC2D00B9";
+        private const string aggroSignature64   = "3B01E8948E3800488D0D";
         private const int charmapOffset32 = 0;
         private const int charmapOffset64 = 0;
         private const int targetOffset32  = 88;
@@ -239,7 +239,7 @@ namespace Tamagawa.EnmityPlugin
             _overlay.LogDebug("targetAddress: 0x{0:X}", targetAddress.ToInt64());
             _overlay.LogDebug("aggroAddress: 0x{0:X}", aggroAddress.ToInt64());
 
-            if (success)
+            if (success || charmapAddress != IntPtr.Zero)
             {
                 Combatant c = GetSelfCombatant();
                 if (c != null)
